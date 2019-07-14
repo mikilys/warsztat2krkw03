ac192222a82e8afcdf671d5e44a4a9f69e424113
Mikolaj Lyskowski <lyskowski.m@gmail.com>
2019-07-14 15:53:43 +0200

    poczatek kodzenia

---
+++ sql/db.sql
@@ -0,0 +1,12 @@
+-- CREATE DATABASE warsztat2krkw03
+-- CHARACTER SET utf8mb4
+-- COLLATE utf8mb4_unicode_ci;
+
+-- DROP TABLE IF EXISTS users;
+-- CREATE TABLE users (
+-- id int AUTO_INCREMENT,
+-- username VARCHAR(255) UNIQUE NOT NULL,
+-- email VARCHAR(255) UNIQUE NOT NULL,
+-- password VARCHAR(255) NOT NULL,
+-- PRIMARY KEY(id)
+-- );
---
+++ src/pl/coderslab/warsztat2krkw03/model/User.java
@@ -0,0 +1,71 @@
+package pl.coderslab.warsztat2krkw03.model;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class User {
+
+    //parametry
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+
+    //konstruktory
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        setPassword(password);
+    }
+
+    public User() {
+    }
+
+    //gettery i settery
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    //metoda Rafala do sprawdzania hasla
+    public boolean isPasswordCorrect (String password) {
+        return BCrypt.checkpw(password, getPassword());
+    }
+
+    //cialo klasy
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
